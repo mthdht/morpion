5891a5968fb3aa2d3976dfe8f9b280765c7eabab
mthdht <mthdht@gmail.com>
2018-01-27 16:37:01 +0100

    made board component

--- src/index.js
+++ src/index.js
@@ -5,14 +5,65 @@ import './index.css';
 class Square extends React.Component {
     render() {
         return (
-            <button className="w3-button">
+            <button className="w3-button w3-block">
                 {this.props.text}
             </button>
         );
     }
 }
 
+class Board extends React.Component {
+    renderSquare(i) {
+        return <Square text={i}/>;
+    }
+
+    render() {
+        const status = 'Next player: X';
+
+        return (
+            <div className="w3-center">
+                <div className="status">{status}</div>
+                <div className="board-row w3-row w3-bottombar">
+                    <div className="w3-third">
+                        {this.renderSquare(0)}
+                    </div>
+                    <div className="w3-third w3-leftbar w3-rightbar">
+                        {this.renderSquare(1)}
+                    </div>
+                    <div className="w3-third">
+                        {this.renderSquare(2)}
+                    </div>
+                </div>
+
+                <div className="board-row w3-row w3-bottombar">
+                    <div className="w3-third">
+                        {this.renderSquare(4)}
+                    </div>
+                    <div className="w3-third w3-leftbar w3-rightbar">
+                        {this.renderSquare(5)}
+                    </div>
+                    <div className="w3-third">
+                        {this.renderSquare(6)}
+                    </div>
+                </div>
+
+                <div className="board-row w3-row">
+                    <div className="w3-third">
+                        {this.renderSquare(7)}
+                    </div>
+                    <div className="w3-third w3-leftbar w3-rightbar">
+                        {this.renderSquare(8)}
+                    </div>
+                    <div className="w3-third">
+                        {this.renderSquare(9)}
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
 ReactDOM.render(
-    <Square text={'X'}/>,
+    <Board />,
     document.getElementById('root')
 );
