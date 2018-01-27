41a31f6f00d0ecda162123f2a29cde65d9312a3a
mthdht <mthdht@gmail.com>
2018-01-27 16:23:05 +0100

    made square component

--- src/index.js
+++ src/index.js
@@ -2,3 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class Square extends React.Component {
+    render() {
+        return (
+            <button className="w3-button">
+                {this.props.text}
+            </button>
+        );
+    }
+}
+
+ReactDOM.render(
+    <Square text={'X'}/>,
+    document.getElementById('root')
+);
